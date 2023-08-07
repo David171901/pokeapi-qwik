a4f83b2d2c8b4da415c4f31778654c59ed5f9e05
David17190159 <david.pino@samishop.com>
2023-08-06 23:14:54 -0500

    ADD: GTM v2

--- src/root.tsx
+++ src/root.tsx
@@ -13,20 +13,31 @@ export default component$(() => {
    * Dont remove the `<head>` and `<body>` elements.
    */
 
+  const scriptGTM = `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-P4JZT3Z9');`
+
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <QwikPartytown forward={['dataLayer.push']} />
+        {/* <!-- Google Tag Manager --> */}
         <script
           async
           type="text/partytown"
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-P4JZT3Z9"
-        />
+        >{scriptGTM}</script>
+        {/* <!-- End Google Tag Manager --> */}
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
       <body lang="en">
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4JZT3Z9"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
